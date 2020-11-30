64b3abd823c9101ca8725e0f2c153a53dc377d94
noah6570 <noah6570@gmail.com>
2020-11-30 23:03:37 +0900

    Initial Commit

---
+++ WorkDonation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkDonation
+//
+//  Created by hoonKim on 2020/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
